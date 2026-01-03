45e6412c806bcd1e6ceed6fe546d23cbb3b4af21
bank-bonus <rkatov13@vk.com>
2026-01-04 00:52:11 +0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import vkBridge from '@vkontakte/vk-bridge';
 import App from './App';
-import './index.css';
 
 // 1. Инициализируем VK Bridge сразу при загрузке
 // isEmbedded() позволяет понять, находимся ли мы внутри приложения ВК
