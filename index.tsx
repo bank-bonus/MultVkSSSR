89a8a0643d01d07f57977b9d9b8f817d90c6dcdc
bank-bonus <rkatov13@vk.com>
2026-01-04 00:27:33 +0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-
-// Initialize VK Bridge
 import vkBridge from '@vkontakte/vk-bridge';
+import App from './App';
+import './index.css';
 
-vkBridge.send('VKWebAppInit');
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+// 1. Инициализируем VK Bridge сразу при загрузке
+// isEmbedded() позволяет понять, находимся ли мы внутри приложения ВК
+if (vkBridge.isEmbedded()) {
+  vkBridge.send('VKWebAppInit')
+    .then((data) => {
+      if (data.result) {
+        console.log('VK Bridge initialized');
+      }
+    })
+    .catch((error) => {
+      console.error('VK Bridge error:', error);
+    });
+} else {
+  console.log('Running outside of VK (APK/Web mode)');
 }
 
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+// 2. Рендерим приложение
+// Мы не оборачиваем его в дополнительные условия, 
+// так как всю логику переключений мы уже вынесли внутрь App.tsx
 root.render(
   <React.StrictMode>
     <App />
