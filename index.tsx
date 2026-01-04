b31c70c9a3a8a14b1c43efcc3b3f2230352b495b
bank-bonus <rkatov13@vk.com>
2026-01-04 22:44:36 +0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,33 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import vkBridge from '@vkontakte/vk-bridge';
 import App from './App';
-
-// 1. Инициализируем VK Bridge сразу при загрузке
-// isEmbedded() позволяет понять, находимся ли мы внутри приложения ВК
-if (vkBridge.isEmbedded()) {
-  vkBridge.send('VKWebAppInit')
-    .then((data) => {
-      if (data.result) {
-        console.log('VK Bridge initialized');
-      }
-    })
-    .catch((error) => {
-      console.error('VK Bridge error:', error);
-    });
-} else {
-  console.log('Running outside of VK (APK/Web mode)');
-}
+import ErrorBoundary from './ErrorBoundary'; // Импортируем наш щит
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-// 2. Рендерим приложение
-// Мы не оборачиваем его в дополнительные условия, 
-// так как всю логику переключений мы уже вынесли внутрь App.tsx
 root.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>
 );
